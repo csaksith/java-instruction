b4872464095f052668d90744858ef0031c31b589
csaksith <saksitca@mail.uc.edu>
2025-03-11 15:13:36 -0400

    add guessing game for java

--- 05GuessingGame/src/GuessGameApp.java
+++ 05GuessingGame/src/GuessGameApp.java
@@ -2,14 +2,17 @@
 import java.util.Random;
 import java.util.Scanner;
 
+import util.MyConsole;
+
 public class GuessGameApp {
 
 	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Guess the Number Game!~");
-		System.out.println("++++++++++++++++++++++++++++++++++++++");
-		System.out.println("\nI'm thinking of a number from 1 to 100.");
-		System.out.println("Try to guess it.");
+		MyConsole.println("Welcome to the Guess the Number Game!~");
+		MyConsole.println("++++++++++++++++++++++++++++++++++++++");
+		MyConsole.println("\nI'm thinking of a number from 1 to 100.");
+		MyConsole.println("Try to guess it.");
 		int min = 1;
 		int max = 100;
 		int guess = 0;
@@ -19,15 +22,40 @@ public class GuessGameApp {
 		
 		while (choice.equalsIgnoreCase("y")) {
 			int theNumber = rand.nextInt(min,max);
-			System.out.println("Debug*** the number is: "+theNumber);
+			MyConsole.println("Debug*** the number is: "+theNumber);
 			while(guess!=theNumber) {
-				System.out.print("Enter number: ");
-				guess = Integer.parseInt(sc.nextLine());
+				guess = MyConsole.promptInt("Guess the number: ", min, max);
 				count++;
 				int diff = guess-theNumber;
+				
 				if (diff==0) {
-					System.out.println("WINNER!!!");
+					MyConsole.println("WINNER!!!");
+					if (count<=3) {
+						MyConsole.println("Great work! You are a mathematical wizard.");
+					}
+					else if (count>3 && count<=7) {
+						MyConsole.println("Not too bad! You've got some potential.");
+					}
+					else if (count>7) {
+						MyConsole.println("What took you so long? Maybe you should take some lessons.");
+					}
 				}
+				else if(diff >=10) {
+					MyConsole.println("Way too high!! Try again!");
+				}
+				else if(diff <-10) {
+					MyConsole.println("Way too low!! Try again!");
+				}
+				else if(diff<=5) {
+					MyConsole.println("You are SO CLOSE!! Try again!");
+				}
+				else if(diff<0) {
+					MyConsole.println("Too low! Try again!");
+				}
+				else if(diff>0) {
+					MyConsole.println("Too high! Try again!");
+				}
+	
 			}
 			
 			
@@ -35,20 +63,19 @@ public class GuessGameApp {
 			choice = sc.nextLine();
 		}
 	
-	
 	System.out.println("\nBye!");
 	sc.close();
 	}
-	private static int promptInt(string prompt) {
-		boolean isValid=false;
+
+	private static int promptInt(String prompt) {
+		boolean isValid = false;
 		int result = 0;
-		while(!isValid) {
-			System.out.println(prompt);
+		while (!isValid) {
+			MyConsole.println(prompt);
 			if (sc.hasNextInt()) {
-				result=Integer.parseInt(sc.nextLine());
-				isValid=true;
-			}
-			else {
+				result = Integer.parseInt(sc.nextLine());
+				isValid = true;
+			} else {
 				System.err.println("Error - invalid entry.");
 				sc.nextLine();
 			}
@@ -56,4 +83,3 @@ public class GuessGameApp {
 		return result;
 	}
 }
-
